acb13bd6f167ccf97fd4875aa1d6b4c1cd01e6b9
Your Name <nikunjshah7778@gmail.com>
2025-02-28 23:07:26 +0530

    razoroay: wip: as of now not needed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const Razorpay = require("razorpay");
+const bodyParser = require("body-parser");
 
 dotenv.config();
 
@@ -37,6 +39,58 @@ app.get("/", (req, res) => {
 // Routes
 const authRoutes = require("./routes/Auth");
 app.use("/api/auth", authRoutes);
+
+app.post("/orders", async (req, res) => {
+  const razorpay = new Razorpay({
+    key_id: "rzp_test_GcZZFDPP0jHtC4",
+    key_secret: "6JdtQv2u7oUw7EWziYeyoewJ",
+  });
+
+  const options = {
+    amount: req.body.amount,
+    currency: req.body.currency,
+    receipt: "receipt#1",
+    payment_capture: 1,
+  };
+
+  try {
+    const response = await razorpay.orders.create(options);
+
+    res.json({
+      order_id: response.id,
+      currency: response.currency,
+      amount: response.amount,
+    });
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.get("/payment/:paymentId", async (req, res) => {
+  const { paymentId } = req.params;
+
+  const razorpay = new Razorpay({
+    key_id: "rzp_test_GcZZFDPP0jHtC4",
+    key_secret: "6JdtQv2u7oUw7EWziYeyoewJ",
+  });
+
+  try {
+    const payment = await razorpay.payments.fetch(paymentId);
+
+    if (!payment) {
+      return res.status(500).json("Error at razorpay loading");
+    }
+
+    res.json({
+      status: payment.status,
+      method: payment.method,
+      amount: payment.amount,
+      currency: payment.currency,
+    });
+  } catch (error) {
+    res.status(500).json("failed to fetch");
+  }
+});
 
 // Start Server
 const PORT = process.env.PORT || 5000;
